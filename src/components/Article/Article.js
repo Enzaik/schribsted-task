517d3039e936b6dbb6866cd51a197d4cadd45c61
enzaik <imernestodiaz@gmail.com>
2019-10-03 11:34:58 +0200

    add more CSS changes

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -6,7 +6,7 @@ const article = ({ title, img, date, preamble }) => {
     return (
         <div className='article'>
             <div className='image'>
-                <img src={img} alt='img' height="42" width="42"></img>
+                <img src={img} alt=':/' height="42" width="42"></img>
             </div>
             <div className='description'>
                 <div className='title'>
