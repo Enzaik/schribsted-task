9a75bf98b269bf47c0ac937b39b0ec46e6795c69
enzaik <imernestodiaz@gmail.com>
2019-10-03 12:43:16 +0200

    Show preamble only in bigger screens and apply more CSS changes

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -3,19 +3,32 @@ import React from 'react';
 import './Article.css'
 
 const article = ({ title, img, date, preamble }) => {
+    console.log(date, 'date')
+    const [day, month, year] = date.split(' ');
+    const newMonth = month.slice(0,3);    
+    const newDate = [day, newMonth, year].join(' ');
+    console.log(newDate, 'month');
+    
     return (
         <div className='article'>
             <div className='image'>
                 <img src={img} alt=':/' height="42" width="42"></img>
             </div>
             <div className='description'>
-                <div className='title'>
-                    {title}
+                <div className='titular'>
+                    <div className='title'>
+                        {title}
+                    </div>
+                    <div className='date'>
+                        {newDate}
+                    </div>
                 </div>
-                <div className='date'>
-                    {date}
+
+                <div className='preamble'>
+                    {preamble}
                 </div>
-                
+
+
             </div>
 
         </div>
